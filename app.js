22e7b893ae1e30bf311fbf81ba6fde38962e53b5
Mirza <mirzataufeeq98436@gmail.com>
2025-03-11 11:51:36 +0530

    new

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import router from './routes/school.route.js'
 import errorHandler from "./middlewares/errorHandler.js";
 import express from 'express'
 import bodyParser from 'body-parser'
@@ -9,11 +10,10 @@ app.use(cors({      //middleware
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-app.use(bodyParser.json());  //middleware for enable json parsing
 app.use(express.json())
+app.use(bodyParser.json());  //middleware for enable json parsing
 app.use(morgan("dev"));      //morgan middleware log http request
 
-import router from './routes/school.route.js'
 app.use('/api/v1/schools',router)
 app.use(errorHandler);
 export {app}
